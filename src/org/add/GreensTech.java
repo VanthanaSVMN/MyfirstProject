1e9c2dc406dd6c4b3893535d5eca82e63a55105c
VanthanaSVMN <vanthanasvmn@gmail.com>
2024-11-16 00:51:37 +0530

    myfirstproject Commit

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,39 @@
+package org.add;
+
+public class GreensTech {
+	
+	private void greensOmr() {
+		
+		System.out.println("Welcome to OMR");
+		
+	}
+	private void greensAdyar() {
+		
+		System.out.println("Welcome to adayar");
+		
+	}
+	private void greensTambaram() {
+		
+		System.out.println("Welcome to Tambaram");
+		
+	}
+	
+	private void greensVelacherry() {
+		
+		System.out.println("Welcome to Velacherry");
+		
+	}
+	private void greensAnnaNagar() {
+		
+		System.out.println("Welcome to annanagar");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		GreensTech A = new GreensTech();
+		A.greensOmr();
+		A.greensAnnaNagar();
+	}
+
+}
